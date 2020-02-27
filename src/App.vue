700bf3687b8fb5aa0a480033cd74fc5f8b1539a7
Cody Squadroni <codysquadroni@weber.edu>
2020-02-27 08:26:31 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
     <!--Bind data props maxQuotes and set it equal to the maxQuotes created below-->
 
     <!--ToDo: Use the NewQuote component selector-->
-    <!--Initiate an $emit event listener called quoteAdded that is equal to newQuote-->
+    <!--Call @quoteAdded that is emitted from the NewQuote Component and pass the newQuote method created below-->
 
     <!--ToDo: Use the QuoteGrid component selector-->
     <!--Bind data props quotes and set it equal to the quotes array created below-->
-    <!--Call quoteDeleted that is emitted from the QuoteGrid Component and pass the deleteQuote method created below-->
+    <!--Call @quoteDeleted that is emitted from the QuoteGrid Component and pass the deleteQuote method created below-->
     <div class="row">
       <div class="col-sm-12 text-center">
         <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
@@ -23,19 +23,19 @@
 export default {
   data () {
     return {
-      // ToDo: Create an array data object called quotes
-      // Have at least one string quote by default
+      // ToDo: Create an array called quotes
+      // Have at least one string by default
 
-      // ToDo: Create a data object maxQuotes that is set to 10
+      // ToDo: Create maxQuotes that is set to 10
     }
   },
   methods: {
-    // ToDo: Create a method that checks when a newQuote is created and takes the parameter quote.
-    // This method will check if the quotes (Array Data Object) length is greater than or equal to the max quotes allowed
+    // ToDo: Create a method called newQuote that takes the parameter quote.
+    // This method will check if the quotes length is greater than or equal to the maxQuotes
     // If it is return an alert telling the user to delete a quote
     // Outside of conditional, push quote (method parameter) to the quotes array
 
-    // ToDo: Create a method deleteQuote that takes in a parameter of index
+    // ToDo: Create a method called deleteQuote that takes in a parameter of index
     // This method will splice the quotes array by (index, 1)
   },
   components: {
